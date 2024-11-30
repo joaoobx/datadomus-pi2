0b78e71c44dd293bcc3e06ba2e07318e4c1d511a
João Marcos de Oliveira Barbosa <joaoobx@gmail.com>
2024-11-30 20:16:55 -0300

    Adding list all

--- datadomus-backend/src/main/java/br/com/datadomus/modules/residents/usecases/getresidents/GetResidentsController.java
+++ datadomus-backend/src/main/java/br/com/datadomus/modules/residents/usecases/getresidents/GetResidentsController.java
@@ -4,7 +4,6 @@ import br.com.datadomus.modules.residents.entities.Resident;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
