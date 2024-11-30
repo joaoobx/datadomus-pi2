b23b70172f35ec992cf1e76fa4f74f6fd2b31e51
João Marcos de Oliveira Barbosa <joaoobx@gmail.com>
2024-11-30 15:17:58 -0300

    adding swagger

--- datadomus-backend/src/main/java/br/com/datadomus/DatadomusApplication.java
+++ datadomus-backend/src/main/java/br/com/datadomus/DatadomusApplication.java
@@ -2,8 +2,10 @@ package br.com.datadomus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class DatadomusApplication {
 
     public static void main(String[] args) {
